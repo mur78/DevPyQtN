b5d8d5f66432b55ae41abb30e499bad3480ab8ab
Mur78 <drivem78@mail.ru>
2022-06-21 19:37:25 +0300

    commit game 2048 form 21.06

--- scripts/P_2048/MyWindow2048.py
+++ scripts/P_2048/MyWindow2048.py
@@ -1,6 +1,12 @@
 from PySide2 import QtCore, QtWidgets, QtGui
 from ui import form_2048
+from Sim_2048 import Simulate2048
 
+# matrix = [[0, 0, 0, 0],
+#          [0, 0, 0, 0],
+#          [0, 0, 0, 0],
+#          [0, 0, 0, 0]]
+m = Simulate2048()
 
 # noinspection PyCallingNonCallable
 class MyWidgetsForm2048(QtWidgets.QMainWindow):
@@ -9,58 +15,32 @@ class MyWidgetsForm2048(QtWidgets.QMainWindow):
         self.ui = form_2048.Ui_MainWindow()
         self.ui.setupUi(self)
         self.setMouseTracking(True)
-        self.ui.lineEdit_1.setText(self.getdata())
-        self.ui.lineEdit_2.setText(self.getdata())
-        self.ui.lineEdit_3.setText(self.getdata())
-        self.ui.lineEdit_4.setText(self.getdata())
-        self.ui.lineEdit_5.setText(self.getdata())
-        self.ui.lineEdit_6.setText(self.getdata())
-        self.ui.lineEdit_7.setText(self.getdata())
-        self.ui.lineEdit_8.setText(self.getdata())
-        self.ui.lineEdit_9.setText(self.getdata())
-        self.ui.lineEdit_10.setText(self.getdata())
-        self.ui.lineEdit_11.setText(self.getdata())
-        self.ui.lineEdit_12.setText(self.getdata())
-        self.ui.lineEdit_13.setText(self.getdata())
-        self.ui.lineEdit_14.setText(self.getdata())
-        self.ui.lineEdit_15.setText(self.getdata())
-        self.ui.lineEdit_16.setText(self.getdata())
-        self.ui.right.clicked.connect(self.click_right)
-        self.ui.left.clicked.connect(self.click_left)
-        self.ui.down.clicked.connect(self.click_down)
-        self.ui.up.clicked.connect(self.click_up)
-        self.ui.init.clicked.connect(self.click_init)
-        self.ui.lineEdit_score.setText(self.total_score())
 
-    def getdata(self):
-        value_str = "5"
-        return value_str
-
-    def total_score(self):
-        total_str = "2048"
-        return total_str
-
-    def click_init(self):
-        pass
-
-    def click_right(self):
-        pass
-
-    def click_left(self):
-        pass
-
-    def click_up(self):
-        pass
-
-    def click_down(self):
-        pass
-
-
-
+        self.ui.lineEdit_1.setText(str(m.matrix[0][0]))
+        self.ui.lineEdit_2.setText(str(m.matrix[0][1]))
+        self.ui.lineEdit_3.setText(str(m.matrix[0][2]))
+        self.ui.lineEdit_4.setText(str(m.matrix[0][3]))
+        self.ui.lineEdit_5.setText(str(m.matrix[1][0]))
+        self.ui.lineEdit_6.setText(str(m.matrix[1][1]))
+        self.ui.lineEdit_7.setText(str(m.matrix[1][2]))
+        self.ui.lineEdit_8.setText(str(m.matrix[1][3]))
+        self.ui.lineEdit_9.setText(str(m.matrix[2][0]))
+        self.ui.lineEdit_10.setText(str(m.matrix[2][1]))
+        self.ui.lineEdit_11.setText(str(m.matrix[2][2]))
+        self.ui.lineEdit_12.setText(str(m.matrix[2][3]))
+        self.ui.lineEdit_13.setText(str(m.matrix[3][0]))
+        self.ui.lineEdit_14.setText(str(m.matrix[3][1]))
+        self.ui.lineEdit_15.setText(str(m.matrix[3][2]))
+        self.ui.lineEdit_16.setText(str(m.matrix[3][3]))
+        self.ui.right.clicked.connect(m.click_right())
+        self.ui.left.clicked.connect(m.click_right())
+        self.ui.down.clicked.connect(m.click_down())
+        self.ui.up.clicked.connect(m.click_up())
+        self.ui.init.clicked.connect(m.click_init())
+        self.ui.lineEdit_score.setText(m.total_score())
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
     myapp = MyWidgetsForm2048()
     myapp.show()
-
     app.exec_()
---
+++ scripts/P_2048/Sim_2048.py
@@ -0,0 +1,30 @@
+
+class Simulate2048:
+
+    def __init__(self):
+        self.matrix = [[0, 0, 0, 0],
+                       [0, 0, 0, 0],
+                       [0, 0, 0, 0],
+                       [0, 0, 0, 0]]
+
+    def total_score(self):
+        total_str = "2048"
+        return total_str
+
+    def click_init(self):
+        pass
+
+    def click_right(self):
+        pass
+
+    def click_left(self):
+        pass
+
+    def click_up(self):
+        pass
+
+    def click_down(self):
+        pass
+
+    def usr_win(self):
+        pass
